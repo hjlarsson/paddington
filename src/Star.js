fd612d3e1f7b240669fe5ff7c6a413ece148e3d8
Håkan Rosenhorn <hrosenhorn@ea.com>
2015-05-13 13:13:58 +0200

    Cleanup

--- src/Star.js
+++ src/Star.js
@@ -2,7 +2,6 @@ function Star(game, x, y) {
     Phaser.Sprite.call(this, game, x, y, 'star');
 
     this.game = game;
-    this.sprite = null;
     this.anchor.setTo(0.5, 0.5);
     this.game.physics.enable(this, Phaser.Physics.ARCADE);
 
